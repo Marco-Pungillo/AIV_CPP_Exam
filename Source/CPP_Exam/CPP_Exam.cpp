a2374011dbb00ee28bab65126a582e3d98ab8cfb
Marco-Pungillo <pungillomarco@gmail.com>
2024-05-03 17:28:39 +0200

    removed everything

--- Source/CPP_Exam/CPP_Exam.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CPP_Exam.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CPP_Exam, "CPP_Exam" );
- 
